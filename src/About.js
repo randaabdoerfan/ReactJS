f20da9e75e6d278c016edfb9ac984c9b6628553a
Randa Erfan <157633836+randaabdoerfan@users.noreply.github.com>
2024-07-30 16:56:13 +0300

    Add files via upload

---
+++ src/About.js
@@ -0,0 +1,14 @@
+export default function About(){
+    return(
+        <div style={{width:"100%",
+            height:"70px",
+            display:"flex",
+            backgroundColor:"gray",
+            justifyContent:"center",
+            position:"fixed",
+        }}>
+            <h3> About</h3>
+
+        </div>
+    )
+}
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+export default function Header(){
+    return(
+        <div style={{width:"100%",
+            height:"70px",
+            display:"flex",
+            backgroundColor:"gray",
+            justifyContent:"center",
+            position:"fixed",
+            
+        }}>
+            <h3> Header</h3>
+
+        </div>
+    )
+}
